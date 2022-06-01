89d2c91d684edc5745dcd769be6387625f93d7f8
J05hL <josh.langford@hotmail.co.uk>
2022-06-01 22:32:46 +0100

    correction 2.3 complete

--- task-2/routes/users.test.js
+++ task-2/routes/users.test.js
@@ -47,12 +47,8 @@ describe(`when get req to /users returns 200, a res with body structured correct
     //const actual = [{ id: 1, username: `Josh` }, { id: 2, username: `JJOSH` }, /*{ id: true, username: 1n }*/]
     const expected = { id: expect.any(Number), username: expect.any(String) };
     actual.forEach(e => expect(e).toEqual(expected));
-  })
-
-
+  });
 })
-
-
 
 /*
 Write an asynchronous test (in routes/users.test.js) which:
@@ -60,7 +56,25 @@ Write an asynchronous test (in routes/users.test.js) which:
 Sends a GET /users/4 request to our app using Supertest
 Checks if the response's HTTP status code is 200
 Checks if the response's body is an object with the structure { success: true, payload: { id: 4, username: any string } }
+*/
 
+describe(`when get req to /users/4 returns 200 and a res with body structured correctly`,
+() => {
+  test(`when get req to /users/4 returns 200 status code`,
+  async () => {
+    const response = await request(app).get(`/users/4`);
+    expect(response.statusCode).toBe(200);
+  });
+  test(`a res with body structured { success: true, payload: { id: 4, username: any string } is returned`,
+  async () => {  
+    const response = await request(app).get(`/users/4`);
+    const actual = response.body;
+    const expected = { success: true, payload: { id: 4, username: expect.any(String) }};
+    expect(actual).toEqual(expected);
+  });
+})
+
+/*
 Write an asynchronous test (in routes/users.test.js) which:
 
 Sends a GET /users/99 request to our app using Supertest

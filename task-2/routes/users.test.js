3f75a703f2941c2cccb5be3cbab03a117714154d
wanteng <wantengteoh@gmail.com>
2022-06-04 21:32:55 +0100

    task2.3 done

--- task-2/routes/users.test.js
+++ task-2/routes/users.test.js
@@ -1,1 +1,23 @@
-// Write your tests for task 2 in this file. Plan out what you need to import/require.
+import request from "supertest";
+import app from "../app.js";
+import {test,expect} from "@jest/globals";
+
+test(`check correct request path, response structure, payload structure`, async function () {
+    
+    const response = await request(app).get("/users");
+    const expected = {
+        body:{ 
+            success: true, 
+            payload: { id: expect.arrayContaining([expect.any(Number)]), 
+                username: expect.arrayContaining([expect.any(String)]) }
+             }
+
+    }
+    
+    //check reuquest path is 200
+    expect(response.statusCode).toBe(200);
+    expect(response._body).toStrictEqual(expectedBody);
+    expect(response._body.payload).toStrictEqual(expect.arrayContaining(expectedPayload));
+
+
+});
